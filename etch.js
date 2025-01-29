f704bc4b4f90e4edf610a60718da1aa929d7c3c2
Dylan M <dmurayama93@gmail.com>
2025-01-28 16:41:55 -1000

    creates function that makes one unit of the grid

---
+++ etch.js
@@ -0,0 +1,22 @@
+//create 16x16 grid
+//add hover effect to change color of grid squares
+
+//bonus:
+//take input for grid dimensions
+
+//create 1 box
+const gridContainer = document.querySelector(".grid-container")
+
+function createGrid(xAxis = 16, yAxis = 16){
+    //set dimensions of grid based on x and y axis
+    for(i = 0; i < xAxis; i++){
+        
+    }
+}
+
+function createBox(){
+    const box = document.createElement('div');
+    gridContainer.appendChild(box);
+}
+
+createBox();
