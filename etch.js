3152e6531712e8f8ba208470ca4c677e3a81b5bf
Dylan M <dmurayama93@gmail.com>
2025-01-29 22:47:45 -1000

    cleans up unused code and comments

--- etch.js
+++ etch.js
@@ -1,10 +1,7 @@
 const gridContainer = document.querySelector(".grid-container")
 
 function createGrid(xAxis = 16, yAxis = 16){
-    //set dimensions of grid based on x and y axis
-    // gridContainer.style.height = `${1000 + 'px'}`;
     for(i = 0; i < yAxis; i++){
-        //create row
         const gridRow = document.createElement('div');
         gridRow.classList.toggle('grid-row');
         
@@ -19,33 +16,9 @@ function createGrid(xAxis = 16, yAxis = 16){
     }
 }
 
-function createRow(){
-    const gridRow = document.createElement('div');
-    gridRow.classList.toggle('gridRow');
-    gridContainer.appendChild(gridRow);
-}
-
-function createBox(){
-    const box = document.createElement('div');
-    box.classList.toggle('initialStyle');
-    box.style.width = `${gridContainer.style.width / 16}px`;
-    box.style.height = box.style.width;
-    gridContainer.appendChild(box);
-}
-
-//set max width and height of box
-function setBoxDimensions(xAxis = 16, box){
-    //take dimensions of container
-    //divide by xAxis or yAxis
-    //max width and height = maxwidth / xAxis
-    //width = parent width / xAxis
-    
-    console.log(gridContainer.style.width);
-    console.log(box.style.width);
-} 
-
 createGrid();
 
+//adds event listener to all divs in grid
 const boxes = document.querySelectorAll('.initialStyle');
 boxes.forEach((boxDiv) => {
     boxDiv.addEventListener('mouseover', function(e){
