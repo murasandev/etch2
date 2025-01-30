7baf339f8d23ddf65e42253d65326b56a37e6207
Dylan M <dmurayama93@gmail.com>
2025-01-30 09:41:30 -1000

    adds hover effect to newly created grid

--- etch.js
+++ etch.js
@@ -1,14 +1,14 @@
 const gridContainer = document.querySelector(".grid-container")
 
-function createGrid(xAxis = 16, yAxis = 16){
-    for(i = 0; i < yAxis; i++){
+function createGrid(axisSize = 16){
+    for(i = 0; i < axisSize; i++){
         const gridRow = document.createElement('div');
         gridRow.classList.toggle('grid-row');
         
         gridContainer.appendChild(gridRow);
-        gridRow.style.height = `${(500 / xAxis)}px`;
+        gridRow.style.height = `${(500 / axisSize)}px`;
 
-        for(j=0; j < xAxis; j++){
+        for(j=0; j < axisSize; j++){
             const box = document.createElement('div');
             box.classList.toggle('initialStyle');
             gridRow.appendChild(box);
@@ -17,19 +17,24 @@ function createGrid(xAxis = 16, yAxis = 16){
 }
 
 createGrid();
+addHoverEffect();
 
-//adds event listener to all divs in grid
-const boxes = document.querySelectorAll('.initialStyle');
-boxes.forEach((boxDiv) => {
-    boxDiv.addEventListener('mouseover', function(e){
-        e.target.style.background = 'black';
-    });     
-});
+function addHoverEffect(){
+    //adds event listener to all divs in grid
+    const boxes = document.querySelectorAll('.initialStyle');
+    boxes.forEach((boxDiv) => {
+        boxDiv.addEventListener('mouseover', function(e){
+            e.target.style.background = 'black';
+        });     
+    });
+}
+
 
 const btnGrid = document.querySelector('.btn-grid');
 btnGrid.addEventListener('click', function(){
     do{
         xAxis = prompt("Enter a number between 16 and 100");
+        createGrid(xAxis);
+        addHoverEffect();
     }while(xAxis <  16 || xAxis > 100);
-    console.log(xAxis);
 });
